594edf8f5365c76f5f38aad9b5e91ec0cba1e93b
afiamoah <afiamoah90@gmail.com>
2022-07-26 21:01:37 -0700

    eslint file modified and menu made similar to design

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-/* eslint-disable */
-const MobileMenu = document.querySelector(".hamburger");
-const MenuItems = document.querySelector(".nav-container");
-
-MobileMenu.addEventListener("click", function () {
-    MobileMenu.classList.toggle("active");
-    MenuItems.classList.toggle("active");
+const MobileMenu = document.querySelector('.hamburger');
+const MenuItems = document.querySelector('.nav-container');
+MobileMenu.addEventListener('click', () => {
+  MobileMenu.classList.toggle('active');
+  MenuItems.classList.toggle('active');
 });
-const Links = document.querySelectorAll(".nav-items");
-for (var x = 0; x < Links.length; x++) {
-    Links[x].addEventListener("click", () => {
-        MobileMenu.classList.remove("active");
-        MenuItems.classList.remove("active");
-    });
+const Links = document.querySelectorAll('.nav-items');
+for (let x = 0; x < Links.length; x += 1) {
+  Links[x].addEventListener('click', () => {
+    MobileMenu.classList.remove('active');
+    MenuItems.classList.remove('active');
+  });
 }
-
