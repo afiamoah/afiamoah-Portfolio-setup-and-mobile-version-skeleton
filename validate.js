3617840bcddb5e8061d58ac8f5a2fd3a8fa82276
afiamoah <afiamoah90@gmail.com>
2022-07-30 14:31:03 -0700

    contact form validated to check email

--- validate.js
+++ validate.js
@@ -1,6 +1,7 @@
 const submitbutton = document.querySelector('.form-inputs');
 const EmailInput = document.querySelector('.form-email');
-const ShowMessage = document.querySelector('.email-checker');
+const ShowMessage = document.querySelector('.email-checks');
+
 // eslint-disable-next-line no-useless-escape
 const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
